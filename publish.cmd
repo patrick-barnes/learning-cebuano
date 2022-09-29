5de40fda92f4d8a1f7152b5274dfe0b590bda10d
patrick-barnes <pjbarnes@gmail.com>
2022-09-29 13:00:07 -0500

    'publish'

---
+++ publish.cmd
@@ -0,0 +1,1 @@
+ng build --base-href https://patrick-barnes.github.io/learning-cebuano/ --output-path docs
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,10 @@ export class AppComponent {
   public isInitialized: boolean = false;
   public searchResults: any[] = [];
   public searchText: string = "crazy";
-  public doC: boolean = false;
+  public doC: boolean = true;
   public doE: boolean = true;
   public examplesOnly: boolean = true;
-  public wholeWordOnly: boolean = true;
+  public wholeWordOnly: boolean = false;
 
   constructor(private http: HttpClient) { }
 
@@ -57,17 +57,12 @@ export class AppComponent {
       let entries = this.dictionary[root];
       for (let entry of entries) {
         entry.root2 = this.searchableCebuano(entry.root);
-        //entry.root = null; // temp
         entry.form2 = this.searchableCebuano(entry.form);
-        //entry.form = null; // temp
         entry.meaning2 = this.searchableEnglish(entry.meaning);
-        //entry.meaning = null; // temp
         if (entry.examples) {
           for (let example of entry.examples) {
             example.c2 = this.searchableCebuano(example.c);
-            //example.c = null; // temp
             example.e2 = this.searchableEnglish(example.e);
-            //example.e = null; // temp
           }
         }
       }
@@ -84,30 +79,37 @@ export class AppComponent {
   }
 
   public doSearch(s: string, doC: boolean, doE: boolean, examplesOnly: boolean, wholeWordOnly: boolean) {
+    console.log('doSearch(): s=' + s + ', doC=' + doC + ', doE=' + doE + ', examplesOnly=' + examplesOnly + ', wholeWordOnly=' + wholeWordOnly);
     let sc = this.searchableCebuano(s);
     let se = this.searchableEnglish(s);
     this.searchResults = []; // clear
     for (let root in this.dictionary) {
-      // if (doC && root2.includes(sc)) {
-      //     console.log("[C] [" + root + "] " + root + " => " + dictionary[root][0].meaning + " ...");
-      // }
       let entries = this.dictionary[root];
+      // let mainEntry = entries[0];
+      // let root2 = mainEntry.root2;
+      // if (doC && root2.includes(sc)) {
+      //   console.log("[C] [" + root + "] " + root + " => " + mainEntry.meaning + " ...");
+      //   this.addSearchResult(root, mainEntry.meaning + " ...", root, mainEntry.meaning);
+      // }
+      // if (doE && mainEntry.meaning.includes(se)) {
+      //   console.log("[C] [" + root + "] " + root + " => " + mainEntry.meaning + " ...");
+      //   this.addSearchResult(root, mainEntry.meaning + " ...", root, mainEntry.meaning);
+      // }
       for (let entry of entries) {
-        // if (!examplesOnly && doC && entry.form2 && entry.form2.includes(sc)) {
-        //   console.log("[C] [" + root + "] " + entry.form + " => " + entry.meaning);
-        // }
-        // if (!examplesOnly && doE && entry.meaning2 && entry.meaning2.includes(se)) {
-        //   console.log("[E] [" + root + "] " + entry.form + ":" + entry.pos + ":" + entry.number + " => " + entry.meaning);
-        // }
+        if (!examplesOnly && doC && entry.form2 && entry.form2.includes(sc)) {
+          this.addSearchResult(root, this.dictionary[root][0].meaning + " ...", entry.form, entry.meaning);
+        }
+        if (!examplesOnly && doE && entry.meaning2 && entry.meaning2.includes(se)) {
+          this.addSearchResult(root, this.dictionary[root][0].meaning + " ...", entry.form, entry.meaning);
+        }
         if (entry.examples) {
           for (let example of entry.examples) {
             if (wholeWordOnly) {
               if (doE && example.e2 && example.e2.match('\\b' + se + '\\b')) {
                 this.addSearchResult(root, this.dictionary[root][0].meaning + " ...", example.c, example.e);
-                // console.log("[" + root + "] " + root + " => " + this.dictionary[root][0].meaning + " ...");
-                // console.log(" C: " + example.c);
-                // console.log(" E: " + example.e);
-                // console.log();
+              }
+              if (doC && example.c2 && example.c2.match('\\b' + sc + '\\b')) {
+                this.addSearchResult(root, this.dictionary[root][0].meaning + " ...", example.c, example.e);
               }
             } else {
               if (doC && example.c2 && example.c2.includes(sc)) {
@@ -121,5 +123,10 @@ export class AppComponent {
         }
       }
     }
+  }
+
+  public onSearch($event: any) {
+    console.log("onSearch(): $event:", $event);
+    this.doSearch(this.searchText, this.doC, this.doE, this.examplesOnly, this.wholeWordOnly);
   }
 }
