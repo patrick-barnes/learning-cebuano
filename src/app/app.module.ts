0e0cd40fa5ccd7b7f9854eb148afad1d04c9716e
patrick-barnes <pjbarnes@gmail.com>
2026-09-20 15:13:54 -0500

    upgrade from angular 17 to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WordRootComponent } from './word-root/word-root.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SearchComponent } from './search/search.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    WordRootComponent,
-    SearchComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        WordRootComponent,
+        SearchComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
